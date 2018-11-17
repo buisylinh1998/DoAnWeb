0026efdaf9546abfa08cd8408eef77973a869073
John Smith <wacui113@gmail.com>
2018-11-17 15:51:51 +0700

    Tạo navigation bar

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+    <meta property="og:title" content="KLH">
+    <title>KLH</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+    <!-- Thanh Menu theo style Tân Doanh -->
+   <!-- <div>
+       <a href="index.php">
+           <img src="" alt="KLH">
+       </a>
+
+   </div>
+    <br> -->
+
+    <nav class="d-flex flex-auto flex-justify-between" style="background-color: #28303e;">
+        <div class="d-flex flex-auto container">
+            <ul>
+                <li>
+                    <a href="index.php">
+                        <img src="" alt="KLH">
+                    </a>
+                </li>
+                <li>
+                    <a href="#" title="NHÀ SẢN XUẤT">NHÀ SẢN XUẤT</a>
+                    <ul>
+                        <li>
+                            <a href="#" title="INTEL">INTEL</a>
+                        </li>
+                        <li>
+                            <a href="#" title="MSI">MSI</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" title="LOẠI LINH KIỆN">LOẠI LINH KIỆN</a>
+                    <ul>
+
+                    </ul>
+                </li>
+                 <li>
+                    <div style="height: auto;">
+                        <input type="text" name="" id="" placeholder="search..." style="margin: 10px 0px; height: auto; width: 90%;">
+                    </div>
+                </li>
+                <!--<li>
+                    <a href="#" title="SẢN PHẨM KHÁC">SẢN PHẨM KHÁC</a>
+                    <ul>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" title="BLOG">BLOG</a>
+                    <ul>
+
+                    </ul>
+                </li> -->
+            </ul>
+        </div>
+        <div class="flex-justify-between">
+            <ul>
+                <li>
+                    <a href="">ĐĂNG NHẬP</a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="" alt="cart">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section>
+
+    </section>
+
+</body>
+</html>
